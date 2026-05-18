5214682e812f21ef365ca7f38afe5b6210311f8a
gitGutBro <osero6110@gmail.com>
2026-05-18 18:37:40 +0300

    Added player camera movement

--- Assets/_Project/Logic/NewInputService.cs
+++ Assets/_Project/Logic/NewInputService.cs
@@ -7,9 +7,16 @@ namespace _Project.Logic
     {
         private readonly InputSystem_Actions _actions = new();
 
-        public NewInputService() => 
+        public NewInputService()
+        {
             _actions.Enable();
-        
+            
+            _actions.Player.Look.performed += callbackContext => Look?.Invoke(callbackContext.ReadValue<Vector2>());
+            _actions.Player.Look.canceled += _ => Look?.Invoke(Vector2.zero);
+        }
+
+        public event Action<Vector2> Look; 
+            
         public Vector2 Moved => _actions.Player.Move.ReadValue<Vector2>();
         
         public void Dispose() => 
