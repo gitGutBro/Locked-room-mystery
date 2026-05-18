e80a95ff5a666b3758cf607212d0048b98de5e51
gitGutBro <osero6110@gmail.com>
2026-05-19 00:18:04 +0300

    Added simple dialogue and his service

---
+++ Assets/_Project/Logic/DialogueService.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+namespace _Project.Logic
+{
+    internal class DialogueService : MonoBehaviour
+    {
+        [SerializeField] private PlayerCamera _playerCamera;
+        
+        // Don't change names and signatures "OnConversationStart" and "OnConversationEnd"
+        private void OnConversationStart(Transform actor)
+        {
+            _playerCamera.enabled = false;
+            
+            Transform conversant = DialogueManager.CurrentConversant;
+
+            if (conversant is null)
+                FocusAtConversant(conversant);
+            
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        private void OnConversationEnd(Transform actor)
+        {
+            _playerCamera.enabled = true;
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void FocusAtConversant(Transform conversant)
+        {
+            Vector3 direction = conversant.position - _playerCamera.Transform.position;
+            _playerCamera.Transform.rotation = Quaternion.LookRotation(direction);
+        }
+    }
+}
